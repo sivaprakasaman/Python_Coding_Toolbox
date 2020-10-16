4d924e5058d0f6147a80b0c4ae27d7370d308b40
sivaprakasaman <andrew.n.sivaprakasam@gmail.com>
2020-10-16 17:39:05 -0400

    Added/todos/checked mult freq

--- signal_processing/fft_tools.py
+++ signal_processing/fft_tools.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tone_generator import pure_tone_complex
 
+#TODO Fix normalization
+
 def get_dft(sig, fs, nfft, type):
     fft_c = np.fft.fft(sig)
     fft_n = np.absolute(fft_c)
@@ -9,11 +11,11 @@ def get_dft(sig, fs, nfft, type):
     return [freq,fft_n]
 
 #Implementation of get_fft fxn
-freq_Hz = [100];
+freq_Hz = [100,200];
 fs_Hz = 44e3;
 dur_sec = 1;
-amp = [1];
-phi = [0];
+amp = [1,.5];
+phi = [0,0];
 
 tone = pure_tone_complex(freq_Hz, fs_Hz, dur_sec, amp, phi)
 dft = get_dft(tone[1],fs_Hz,None,'None')
