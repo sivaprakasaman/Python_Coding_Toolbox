583fef8efac4366c39eb68c0a19df401fe5d0315
sivaprakasaman <andrew.n.sivaprakasam@gmail.com>
2021-01-16 02:05:08 -0500

    dimensions fixed, but still not getting expected results

--- signal_processing/timbral_inspection/resynthesize.py
+++ signal_processing/timbral_inspection/resynthesize.py
@@ -4,16 +4,26 @@ import librosa as lb
 import matplotlib.pyplot as plt
 import os
 
-def extract_harmonics(fname, fs = 44100, f_0 = 440, n_harms = 3):
-    x  = lb.load(fname)
-    plt.plot(np.arange(0,len(x[0])), x[0])
-    f_vect = f_0*np.arange(1,n_harms+1);
-    t_vect = np.arange(0,len(x[0]))/fs;
-    temp = np.mat(f_vect).T*np.mat(t_vect);
+def extract_harmonics(fname, fs = 44100, f_0 = 440, n_harms = 6):
+    aud  = lb.load(fname)
+    x = np.array(aud[0])
+    t_vect = np.arange(0,len(x))/fs
+    f_vect = np.arange(1,n_harms+1)*f_0;
+    #comb = np.mat(f_vect).T*np.mat(t_vect);
+    comb = np.multiply(np.asmatrix(f_vect).T,np.asmatrix(t_vect))
+    #print(comb)
+    #x_sin = np.mat(x).T*np.mat(np.sin(2*np.pi*comb))
+    x_sin = np.multiply(np.asmatrix(x),np.sin(2*np.pi*comb))
+    x_cos = np.multiply(np.asmatrix(x),np.cos(2*np.pi*comb))
+    sin_sum = np.sum(x_sin,1);
+    cos_sum = np.sum(x_cos,1);
+    #sin1 = np.squeeze(np.asarray(x_sin[2]))
+    #plt.plot(t_vect,sin1)
 
-    x_sin = np.multiply(np.mat(x[0]),np.mat(np.sin(2*np.pi*temp)))
-    print((t_vect.shape))
-    plt.plot(t_vect,x_sin[0])
+    mags = np.sqrt(np.multiply(sin_sum,sin_sum) + np.multiply(cos_sum,cos_sum))
+    plt.plot(f_vect,np.squeeze(np.asarray(mags)))
+
+
 #############################################################################
 
-extract_harmonics('instruments/violin_A4_normal.mp3');
+extract_harmonics('instruments/viola_A4_normal.mp3');
